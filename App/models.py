0164e8c1efaaad23d0f25806ca5bedfb368d58e5
a1phaaa <lkzxgp@gamil.com>
2023-11-16 20:27:20 +0800

    修改models的Movieinfo

--- App/models.py
+++ App/models.py
@@ -3,13 +3,16 @@ from django.db import models
 
 # Create your models here.
 class MoviesInfo(models.Model):
-    title = models.CharField(max_length=32)
-    director = models.CharField(max_length=32)
-    type = models.CharField(max_length=32)
-    date = models.CharField(max_length=32)
-    rating = models.FloatField()
-    judge = models.FloatField()
-    inq = models.CharField(max_length=64)
+    zh_name = models.CharField(max_length=15)  # 中文名
+    en_name = models.CharField(max_length=70)  # 英文名
+    director = models.CharField(max_length=70)  # 导演
+    actor = models.CharField(max_length=50)  # 演员
+    release_year = models.IntegerField()  # 上映年份
+    release_country = models.CharField(max_length=30)  # 上映国家
+    type = models.CharField(max_length=30)  # 类型
+    score = models.CharField(max_length=3)  # 评分
+    eval_number = models.CharField(max_length=10)  # 评价人数
+    quotation = models.CharField(max_length=50)  # 语录
 
 class Admin(models.Model):
     username = models.CharField(verbose_name="用户名", max_length=32)
